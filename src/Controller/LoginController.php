8b9addb87e39f31712dcc8cb5c812a9540f83cfe
eugenie <eugenie.ragon@gmail.com>
2022-05-25 10:20:18 +0200

    Création du logout

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -23,4 +23,10 @@ class LoginController extends AbstractController
             'error'         => $error,
         ]);
     }
+    #[Route('/logout', name: 'logout', methods: ['GET'])]
+    public function logout()
+    {
+        // controller can be blank: it will never be called!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
 }
